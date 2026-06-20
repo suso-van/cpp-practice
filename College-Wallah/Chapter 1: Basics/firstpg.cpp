82243f39a6e1ca7d8114010fb298892132cb0bdc
susovan <chatterjeesusovan2004@gmail.com>
2026-06-20 18:59:41 +0530

    cpp is loading

---
+++ College-Wallah/Chapter 1: Basics/firstpg.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    cout<<"Hello World!"<<endl;
+    cout<<"for next line "<<"\n";
+    cout<<"for next line ";
+    return 0;
+}
---
+++ College-Wallah/Chapter 1: Basics/printingnum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num = 10;
+    cout << "The number is: " << num << endl;
+    int a = 5, b = 15;
+    cout << "The sum of " << a << " and " << b << " is: " << a + b << endl;
+    return 0;
+}
