33912e8d4142f133f8cd683e2107f0a3d180337e
susovan <chatterjeesusovan2004@gmail.com>
2026-06-21 11:16:54 +0530

    cpp

---
+++ College-Wallah/Chapter 1: Basics/ascii.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >> x;
+    char ch = (char)x;
+    cout << "The ASCII character for " << x << " is: " << ch << endl;
+    return 0;
+}
