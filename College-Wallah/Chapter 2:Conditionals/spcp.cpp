7cc1518bc74f4e6beca71b78e3e9d6ba2c14a983
susovan <chatterjeesusovan2004@gmail.com>
2026-06-21 12:43:52 +0530

    cpp

--- College-Wallah/Chapter 2:Conditionals/spcp.cpp
+++ College-Wallah/Chapter 2:Conditionals/spcp.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int sp,cp;
+    cout<<"Enter the selling price: ";
+    cin>>sp;
+    cout<<"Enter the cost price: ";
+    cin>>cp;
+    if(sp>cp){
+        cout<<"Profit: "<<sp-cp<<endl;  
+    }
+    else if(cp>sp){
+        cout<<"Loss: "<<cp-sp<<endl;
+    }
+    else{
+        cout<<"No profit no loss"<<endl;
+    }
+    return 0;
 }
