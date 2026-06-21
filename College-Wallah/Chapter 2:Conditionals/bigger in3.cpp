bc9636bda337023ffae7dc910b21b65a7dd9c8ad
susovan <chatterjeesusovan2004@gmail.com>
2026-06-21 14:45:54 +0530

    cpp

---
+++ College-Wallah/Chapter 2:Conditionals/bigger in3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter three numbers: ";
+    cin>>a>>b>>c;
+    if(a>b && a>c){
+        cout<<a<<" is the biggest number"<<endl;
+    }
+    else if(b>a && b>c){
+        cout<<b<<" is the biggest number"<<endl;
+    }
+    else{
+        cout<<c<<" is the biggest number"<<endl;
+    }
+    return 0;
+}
